fc2aa31010a67cefa5a0233f071fee7e179f81a1
Alexander <spam.0ff@yandex.ru>
2022-08-18 10:48:23 +0300

    refactoring checkstyle 3

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -6,18 +6,11 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 public interface ItemService {
-
     List<Item> getAllItems(long userId);
-
     void deleteAll();
-
     ItemDto addItem(long userId, ItemDto itemDto);
-
     ItemDto changeItem(ItemDto itemDto, long id, long userId);
-
     ItemDto findItemById(long id);
-
     long deleteItem(long id);
-
     List<Item> searchItems(String text);
 }
